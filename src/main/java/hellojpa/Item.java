ba4fa865d9fa9d2851bebb32efb2682385c19833
mincheolkk <kmc04080408@gmail.com>
2021-09-03 20:44:14 +0900

    Add : 구현 클래스마다 테이블 전략

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,9 +3,11 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)   // 조인 전략을 선택할 수 있음
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @DiscriminatorColumn // D-TYPE 자식 클래스의 엔티티명이 들어가게 됨. ex) DTYPE : Movie
-public class Item {
+public abstract class Item {
+
+    // 추상 클래스로 만들면 Item 에서 따로 만들 수 없음
 
     @Id @GeneratedValue
     private Long id;
