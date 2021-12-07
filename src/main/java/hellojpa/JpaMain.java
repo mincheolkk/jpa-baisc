7e183fb4dde017c6d512d29ee52d2caa753c5400
mincheol <kmc04080408@gmail.com>
2021-12-07 22:32:09 +0900

    test : ci test

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,6 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -21,52 +24,12 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("city1", "street","1000"));
-
-            member.getFavoriteFoods().add("chicken");
-            member.getFavoriteFoods().add("pizza");
-            member.getFavoriteFoods().add("pork");
-
-//            member.getAddressesHistory().add(new Address("old1", "street","1000"));
-//            member.getAddressesHistory().add(new Address("old2", "street","1000"));
-
-            // 값 타입 컬렉션을 일대다 관계 설정으로 바꿈
-            member.getAddressesHistory().add(new AddressEntity("old1", "street", "1000"));
-            member.getAddressesHistory().add(new AddressEntity("old2", "street", "1000"));
+            // 네이티브 SQL
+            em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER")
+                    .getResultList();
 
 
-            em.persist(member);
-            // member 만 persist 했는데 값 타임 컬렉션들은 자동으로 같이 라이프 사이클이 돌아감 .
-            // 값 타입의 라이프 사이클은 member 에 의존함 .
-
-            em.flush();
-            em.clear();
-
-            System.out.println("===========start==========");
-            Member findMember = em.find(Member.class, member.getId());
-
-            // 컬렉션들은 지연로딩
-//            List<Address> addressesHistory = findMember.getAddressesHistory();
-//            for (Address address : addressesHistory) {
-//                System.out.println("address = " + address.getCity());
-//            }
-//
-//            // 값 타입 수정하기
-//            // city -> newCity
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-//
-//            // chicken -> ramen
-//            findMember.getFavoriteFoods().remove("chicken");
-//            findMember.getFavoriteFoods().add("ramen");
-
-            // 기본적으로 컬렉션은 대상을 찾을 때, equals 를 사용함. equals 와 hashcode 가 제대로 구현되어 있어야 함
-//            findMember.getAddressesHistory().remove(new Address("old1", "street", "1000"));
-//            findMember.getAddressesHistory().add(new Address("newCity1", "street", "1000"));
-
-
+            System.out.println("tx = " + tx);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
